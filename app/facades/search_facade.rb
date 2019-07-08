53893b3e6c6425a546009e1726c15a32b3dc9f14
Vince Carollo <vincecarollo@gmail.com>
2019-07-08 09:42:57 -0600

    Add search_facade and faraday connection and request for search results

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -4,6 +4,24 @@ class SearchFacade
   end
 
   def search_results
-    binding.pry
+    conn = Faraday.new(url: 'https://api.nal.usda.gov') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+      faraday.params['api_key'] = ENV['API_KEY']
+      faraday.params['format'] = 'json'
+      faraday.params['sort'] = 'r'
+    end
+
+    response = conn.get('/ndb/search') do |req|
+      req.params['q'] = term
+    end
+
+    food_data = JSON.parse(response.body, symbolize_names: true)
+
+    food_data[:item].map do |food_data|
+      Food.new(food_data)
+    end
   end
+
+  private
+  attr_reader :term
 end
